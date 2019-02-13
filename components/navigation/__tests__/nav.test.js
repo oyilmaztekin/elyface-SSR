dd066892c7d4a17f05843867ed32d7fc395f7ee5
özer yilmaztekin <yilmaztekin.ozer@gmail.com>
2019-02-13 21:55:33 +0300

    improved tests

---
+++ components/navigation/__tests__/nav.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import React from "react";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("Navigation Component Testing", () => {
+  test("should rendered without crashing ", () => {});
+});
