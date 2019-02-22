6b07070f9c136935ce721ff55ed6ed4c0151de63
özer yilmaztekin <yilmaztekin.ozer@gmail.com>
2019-02-22 16:36:48 +0300

    feat: configured axios for dataset

---
+++ configs/API/apis.js
@@ -0,0 +1,13 @@
+export const delivery = {
+  addr: "delivery.blupoint.io",
+  domain_id: "5c4984af5a4da10008e77628",
+  token:
+    "Basic 5c6570c017ff6d000703127b:wBWa98V9RgdT7v9SmhAA"
+};
+
+export const preview = {
+  addr: "preview.blupoint.io",
+  domain_id: "5c4984af5a4da10008e77628",
+  token:
+    "Basic 5c6570c017ff6d000703127b:wBWa98V9RgdT7v9SmhAA"
+};
---
+++ configs/index.js
@@ -0,0 +1,1 @@
+export { delivery, preview } from "./API/apis";
