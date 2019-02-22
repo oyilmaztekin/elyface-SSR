0f1abed434fc3086c203b73206df6da0e03dc422
özer yilmaztekin <yilmaztekin.ozer@gmail.com>
2019-02-22 18:21:34 +0300

    configured next export and build

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,8 @@
 import React from "react";
 import "@globalstyle";
-import Logo from "@comp/logo/";
-import LogoImage from "@static/logo.png";
-import Link from "next/link";
-import LinkButton from "../components/linkButton";
-import Header from "@nest/header/";
 
 const IndexPage = () => (
-  <React.Fragment>
-    <h1>Haberi Yakala SSR Test</h1>
-    <p>SSR Deneme paragraf</p>
-    <Logo
-      src={LogoImage}
-      title="Haberi Yakala Logosu"
-    />
-    <Link href="/about" passHref>
-      <LinkButton
-        text="Deneme"
-        className="link-nav"
-      />
-    </Link>
-    <Header />
-  </React.Fragment>
+  <div>Welcome to next.js!</div>
 );
 
 export default IndexPage;
